634f9fc4c70dab587fcebd87ea2b3073bcb4a6c3
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-02 20:30:37 +0600

    basic login component created in the src>pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/route.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={routes}/>
     </Provider>
   </StrictMode>
 );
