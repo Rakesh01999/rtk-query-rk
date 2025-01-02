0d97bdb0b4012711ccc31af1825086779345d1fa
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-02 20:48:43 +0600

    tasks -created, Link -added to the Navbar

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,5 +1,6 @@
 import App from "@/App";
 import Login from "@/pages/login";
+import Tasks from "@/pages/tasks";
 import Users from "@/pages/users";
 import { createBrowserRouter } from "react-router-dom";
 
