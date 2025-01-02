30119cbe006987e7555a30d511bfbf5dd0cb663d
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-02 21:43:54 +0600

    code modified, theme-provider adding . . .

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,5 +1,4 @@
 import App from "@/App";
-import Login from "@/pages/login";
 import Tasks from "@/pages/tasks";
 import Users from "@/pages/users";
 import { createBrowserRouter } from "react-router-dom";
@@ -18,10 +17,6 @@ const routes = createBrowserRouter([
         element: <Tasks />,
       },
     ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
   },
 ]);
 
