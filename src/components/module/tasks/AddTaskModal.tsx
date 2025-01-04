2bec621ef152c3c1251640e4dad700212b7a655c
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-04 12:30:00 +0600

    Modal added

---
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
+export function AddTaskModal() {
+
+    const form = useForm(),
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+        <FormField
+            control={form.control}
+            name="title"
+            render={() => (
+            <FormItem>
+                <FormLabel />
+                <FormControl>
+                { /* Your form field */}
+                </FormControl>
+            </FormItem>
+            )}
+        />
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
