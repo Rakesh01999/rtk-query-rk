ad868392ef48a50984af116262da994ae8055399
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-09 22:06:21 +0600

    Task Filters created

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,22 +1,45 @@
 import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 export default function Tasks() {
   const tasks = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch();
 
   console.log(tasks);
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h1>Tasks </h1>
-        <Tabs>
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Tasks </h1>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("all"))}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("low"))}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("medium"))}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("high"))}
+              value="High"
+            >
+              high
+            </TabsTrigger>
           </TabsList>
         </Tabs>
         <AddTaskModal />
