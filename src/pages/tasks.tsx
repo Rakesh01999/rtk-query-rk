46b048debf799a5053fae5177630050cd7b0c52e
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-10 23:30:43 +0600

    Connecting the baseApi

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -2,12 +2,15 @@ import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useGetTasksQuery } from "@/redux/api/baseApi";
-import { updateFilter } from "@/redux/features/task/taskSlice";
+import { ITask } from "@/types";
 // import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 export default function Tasks() {
-  const { data, isLoading, isError } = useGetTasksQuery(undefined);
-  console.log({ data, isLoading, isError });
+  const { data, isLoading } = useGetTasksQuery(undefined);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
@@ -15,34 +18,19 @@ export default function Tasks() {
         <h1 className="mr-auto">Tasks </h1>
         <Tabs defaultValue="all">
           <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger
-              value="all"
-            >
-              All
-            </TabsTrigger>
-            <TabsTrigger
-              value="low"
-            >
-              Low
-            </TabsTrigger>
-            <TabsTrigger
-              value="medium"
-            >
-              Medium
-            </TabsTrigger>
-            <TabsTrigger
-              value="High"
-            >
-              high
-            </TabsTrigger>
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="low">Low</TabsTrigger>
+            <TabsTrigger value="medium">Medium</TabsTrigger>
+            <TabsTrigger value="High">high</TabsTrigger>
           </TabsList>
         </Tabs>
         <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
-        {tasks.map((task) => (
-          <TaskCard task={task} key={task.id} />
-        ))}
+        {!isLoading &&
+          data.tasks.map((task: ITask) => (
+            <TaskCard task={task} key={task.id} />
+          ))}
       </div>
     </div>
   );
